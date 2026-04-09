9be5deb918f67853044947fd9fa87f3dd36a0430
Daniel <bannikovdaniel541@gmail.com>
2026-04-10 03:40:39 +0500

    Update logic and hide config

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Твой токен от BotFather (вставь его между кавычек)
-TOKEN = '8798181753:AAGmQgx-LE9IvZShrdee5XzJMSr3XKHNBQM'
+TOKEN = ''
 
 # Твой ключ от GROQ (вставь его между кавычек)
-EnglishRS = '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'
+EnglishRS = ''
 
 # Настройка базы данных (оставляем как есть для SQLite)
 DB_URL = 'sqlite+aiosqlite:///db.sqlite3'
